a06b95ef1525db8d0afb8eefc646700a8e5d81cd
examen semana 16 - 1 <fabianpachao20@gmail.com>
2023-10-13 09:15:44 -0500

    v2 componente definidos

---
+++ src/components/P4variable.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Variable = (mjs) => {
+  return (
+    <div>
+      <h1>Hola mundo... lectura a una variable</h1>
+      <h2>Mensaje: {mjs.xVariable}</h2>
+    </div>
+  )
+}
+
+export default Variable
